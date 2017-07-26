d651f2178cf9379759ecaf1eec71db2496dae01d
Tijmen de Graaff <tijmen76@gmail.com>
2017-07-26 21:35:25 +0200

    setup basic structure for header component

---
+++ app/components/Header/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { View } from "react-native";
+import styles from "./styles";
+
+const Container = () => (
+
+);
+
+export default Header;
