01b682634d7b90fbbeffbecab5089082472abb18
Tijmen de Graaff <tijmen76@gmail.com>
2017-07-26 18:22:05 +0200

    setup file structure for InputButton component

---
+++ app/components/TextInput/InputWithButton.js
@@ -0,0 +1,8 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { View, Text, TouchableHighlight, TextInput } from "react-native";
+import styles from "./styles";
+
+const InputWithButton = () => ();
+
+export default InputWithButton;
