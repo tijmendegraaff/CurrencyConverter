474bcbd19e8eb36ffdc0a33372c577a14e8d0159
Tijmen de Graaff <tijmen76@gmail.com>
2017-07-26 17:36:25 +0200

    setup global stylesheet

--- app/index.js
+++ app/index.js
@@ -1,21 +1,11 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import EStyleSheet from "react-native-extended-stylesheet";
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Let's build our first maybe app!</Text>
-      </View>
-    );
-  }
-}
+// Import own Components
+import Home from "./screens/Home";
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
-  }
+EStyleSheet.build({
+  $primaryBlue: "#4F6D7A"
 });
+
+export default () => <Home />;
