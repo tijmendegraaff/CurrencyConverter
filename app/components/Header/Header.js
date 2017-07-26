b3aa054ece87cf2c4408471d78fd8889c3ca0604
Tijmen de Graaff <tijmen76@gmail.com>
2017-07-26 21:48:17 +0200

    add header component styling

--- app/components/Header/Header.js
+++ app/components/Header/Header.js
@@ -1,10 +1,18 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { View } from "react-native";
+import { View, Image, TouchableOpacity } from "react-native";
 import styles from "./styles";
 
-const Container = () => (
-
+const Header = () => (
+  <View style={styles.container}>
+    <TouchableOpacity style={styles.button}>
+      <Image
+        resizeMode="contain"
+        style={styles.icon}
+        source={require("./images/gear.png")}
+      />
+    </TouchableOpacity>
+  </View>
 );
 
 export default Header;
--- app/screens/Home.js
+++ app/screens/Home.js
@@ -5,6 +5,7 @@ import { Logo } from "../components/Logo";
 import { InputWithButton } from "../components/TextInput";
 import { ClearButton } from "../components/Buttons";
 import { LastConverted } from "../components/Text";
+import { Header } from "../components/Header";
 
 const TEMP_BASE_CURRENCY = "USD";
 const TEMP_QUOTE_CURRENCY = "GBP";
@@ -34,6 +35,7 @@ class Home extends Component {
     return (
       <Container>
         <StatusBar translucent={false} barStyle="light-content" />
+        <Header />
         <Logo />
         <InputWithButton
           buttonText={TEMP_BASE_CURRENCY}
