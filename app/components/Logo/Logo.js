8531973dcedbe565e8b14e676aa197e195070998
Tijmen de Graaff <tijmen76@gmail.com>
2017-07-26 18:12:31 +0200

    add styling to logo component

--- app/components/Logo/Logo.js
+++ app/components/Logo/Logo.js
@@ -1,12 +1,21 @@
 import React from "react";
 import { View, Image, Text } from "react-native";
+import styles from "./styles";
 
 const Logo = () => (
-  <View>
-    <Image source={require("./images/background.png")}>
-      <Image source={require("./images/logo.png")} />
+  <View style={styles.container}>
+    <Image
+      style={styles.containerImage}
+      source={require("./images/background.png")}
+      resizeMode="contain"
+    >
+      <Image
+        style={styles.image}
+        source={require("./images/logo.png")}
+        resizeMode="contain"
+      />
     </Image>
-    <Text>Currency Converter</Text>
+    <Text style={styles.text}>Currency Converter</Text>
   </View>
 );
 
